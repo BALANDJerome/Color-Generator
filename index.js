08e3464b0d9db3cd53707809542ec95dc1b4d149
BALAND Jérome <baland.jerome@gmail.com>
2024-02-16 18:33:27 +0100

    générateur de couleur fonctionnel

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 // Donner au body une couleur de fond en rgb()
 
 // Montrer sur le body la couleur rgb()
+
+// const backgroundBody = document.querySelector("body")
+generateColor = () => {
+  setTimeout(() => {
+    let color1 = [Math.floor(Math.random() * 256)];
+    let color2 = [Math.floor(Math.random() * 256)];
+    let color3 = [Math.floor(Math.random() * 256)];
+    rgb.textContent = `rgb(${color1},${color2},${color3})`;
+    document.body.style.background = rgb.innerText;
+    generateColor()
+  }, 2000)
+}
+generateColor()
