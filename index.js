e0bb7efafcd9ac6194c9b518d1ff29bcb1716e44
BALAND Jérome <baland.jerome@gmail.com>
2024-02-17 11:04:49 +0100

    modif JS avec ajout anim sur btn

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-let pause = true
+// let pause = true
+
+// const generateColor = () => {
+//   if (pause === false) {
+//     let color1 = [Math.floor(Math.random() * 255)];
+//     let color2 = [Math.floor(Math.random() * 255)];
+//     let color3 = [Math.floor(Math.random() * 255)];
+//     rgb.textContent = `rgb(${color1},${color2},${color3})`;
+//     document.body.style.background = rgb.innerText;
+//     setTimeout(() => {
+//       generateColor()
+//     }, 2000)
+//   }
+// }
+// btn.addEventListener("click", () => {
+//   if (pause === true) {
+//     pause = !pause
+//     btn.style.animation = "click 2s forwards";
+//     setTimeout(() => {
+//       btn.textContent = "Stop"
+//     }, 1000)
+//     generateColor()
+//   } else {
+//     pause = !pause
+//     btn.textContent = "start"
+//   }
+// })
 
 const generateColor = () => {
-  if (pause === false) {
-    let color1 = [Math.floor(Math.random() * 255)];
-    let color2 = [Math.floor(Math.random() * 255)];
-    let color3 = [Math.floor(Math.random() * 255)];
-    rgb.textContent = `rgb(${color1},${color2},${color3})`;
-    document.body.style.background = rgb.innerText;
-    setTimeout(() => {
-      generateColor()
-    }, 2000)
-  }
+  let color1 = [Math.floor(Math.random() * 255)];
+  let color2 = [Math.floor(Math.random() * 255)];
+  let color3 = [Math.floor(Math.random() * 255)];
+  rgb.textContent = `rgb(${color1},${color2},${color3})`;
+  document.body.style.background = rgb.innerText;
 }
+const anim = () => {
+  btn.className = "btn"
+  window.requestAnimationFrame(() => {
+    window.requestAnimationFrame(() => {
+      btn.className = "btnAnim"
+    })
+  })
+}
+let interval = null
 btn.addEventListener("click", () => {
-  if (pause === true) {
-    pause = !pause
-    generateColor()
-    btn.textContent = "pause"
+  anim()
+  if (interval === null) {
+    interval = setInterval(generateColor, 2000)
+    setTimeout(() => {
+      btn.textContent = "Stop"
+    }, 1000)
   } else {
-    pause = !pause
-    btn.textContent = "start"
+    clearInterval(interval)
+    interval = null
+    setTimeout(() => {
+      btn.textContent = "Start"
+    }, 1000)
   }
 })
-console.log();
